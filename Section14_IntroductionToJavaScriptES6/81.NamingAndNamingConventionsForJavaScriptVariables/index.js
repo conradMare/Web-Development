78f17b35bf32ae87a202d3670b5b2310fa9ad697
conradMare <conradmare4@gmail.com>
2024-06-03 09:08:18 +0200

    2024/06/03(am)
    S14.81_Naming and Naming Conventions for JavaScript

---
+++ Section14_IntroductionToJavaScriptES6/81.NamingAndNamingConventionsForJavaScriptVariables/index.js
@@ -0,0 +1,18 @@
+// Give meaningful names to variables
+var myName = "Conrad";
+var yourName = "John";
+
+// Can't use keywords as variable names
+// var var = "myVar";
+
+// Variable names CAN'T start with a number, but can contain numbers
+var my123 = 123;
+
+// Variable names CAN'T contain spaces
+// var my name = "name";
+
+// Variable names can only contain Letters, Numbers, Dollar sign, Underscore
+var abc123$_ = "variable naming";
+
+// Use Camel-Case
+var userScoreFinal = 0;
