670ec14204894950422d8011ac1bc3f002d90402
conradMare <conradmare4@gmail.com>
2024-06-03 11:20:04 +0200

    2024/06/03(am)
    S14.89_Functions Part 1: Creating and Calling Functions

---
+++ Section14_IntroductionToJavaScriptES6/89.FunctionsPart1_CreatingAndCallingFunctions/index.js
@@ -0,0 +1,13 @@
+function getMilk() {
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("buyMilk");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+}
+
+getMilk();
