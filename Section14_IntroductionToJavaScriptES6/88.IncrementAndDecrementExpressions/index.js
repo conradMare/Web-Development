09ebc2c392e06019310f106aaa44550e4a249969
conradMare <conradmare4@gmail.com>
2024-06-03 11:02:57 +0200

    2024/06/03(am)
    S14.88_Increment and Decrement Expressions

---
+++ Section14_IntroductionToJavaScriptES6/88.IncrementAndDecrementExpressions/index.js
@@ -0,0 +1,22 @@
+var x = 5;
+x = x + 1;
+
+// Increment:
+x++;
+
+// Decrement:
+x--;
+
+// Increment value by more than one:
+x += 2;
+
+// Increase by the value of another variable:
+var x = 5;
+var y = 3;
+x += y; //8
+
+// Other expressions:
+// +=
+// -=
+// *=
+// /=
