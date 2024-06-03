b43c5de59a23e96cd6eac69697dfef5d08fb438c
conradMare <conradmare4@gmail.com>
2024-06-03 08:01:33 +0200

    2024/06/03(am)
    S14.77_Data Types

---
+++ Section14_IntroductionToJavaScriptES6/77.Data Types/index.js
@@ -0,0 +1,16 @@
+// String -> Text that is not meant to be interpreted as code (String of Characters)
+
+// Numbers -> Doesn't need any special symbols, simply type the numbers
+
+// Boolean -> True/False
+
+// typeof -> Displays the type of data
+
+typeof(123);
+// > "number"
+
+typeof("Conrad");
+// > "string"
+
+typeof(true);
+// > "boolean"
