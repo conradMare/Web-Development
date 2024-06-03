e4c2312224f25f3f17887a421310fcb6b1972973
conradMare <conradmare4@gmail.com>
2024-06-03 10:47:24 +0200

    2024/06/03(am)
    S14.87_Basic Arithmetic and the Modulo Operator in JavaScript

---
+++ Section14_IntroductionToJavaScriptES6/87.BasicArithmeticAndTheModuleOperator/index.js
@@ -0,0 +1,7 @@
+// Dog age to human age converter:
+var dogAge = prompt("How old is your dog?");
+var humanAge = ((dogAge - 2) * 4) + 21;
+alert("Your dog is " + humanAge + " years old in human years");
+
+// Modulo -> Remainder of division
+var a = 9 % 6; //3
