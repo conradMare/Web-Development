ecb5cd6c29cf6846f6a5525c10e70a6ff78832a9
conradMare <conradmare4@gmail.com>
2024-06-05 11:15:29 +0200

    2024/06/05(am)
    Coding Exercise 6: Who's buying lunch? Code Challenge

---
+++ Section15_IntermediateJavaScript/CodingExercise6_WhoIsBuyingLunch/index.js
@@ -0,0 +1,11 @@
+var names = ["Angela", "Ben", "Jenny", "Mike", "Chloe"];
+
+function whosPaying(names) {
+
+    var numberOfPeople = names.length;
+    var randomPersonPosition = Math.floor(Math.random() * numberOfPeople);
+    var randomPerson = names[randomPersonPosition];
+
+    return randomPerson + " is going to buy lunch today!";
+
+}
