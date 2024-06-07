47c785f7116fb730314ba63bc7442f9df9bfbe6e
conradMare <conradmare4@gmail.com>
2024-06-07 07:36:34 +0200

    2024/06/07(am)
    S17.129_Dicee Challenge Step 5: Change both <img> Elements

--- Section17_BossLevelChallenge1-TheDiceeGame/DiceeChallenge-StartingFiles/index.js
+++ Section17_BossLevelChallenge1-TheDiceeGame/DiceeChallenge-StartingFiles/index.js
@@ -7,3 +7,10 @@ var randomImageSource = "images/" + randomDiceImage;
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
