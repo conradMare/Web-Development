e28d5c7ff2255f565106e38907c01cd2fffb95ba
conradMare <conradmare4@gmail.com>
2024-06-10 10:45:24 +0200

    2024/06/10(am)
    S19.158_Website Animations with jQuery

--- Section19_jQuery/jQuery/index.js
+++ Section19_jQuery/jQuery/index.js
@@ -1,32 +1,32 @@
+// $("button").on("click", function() {
+//     $("h1").toggle();
+// });
 
-// Adding an eventListener with jQuery:
-// $("h1").click(function() {
-//     $("h1").css("color", "purple");
-// })
+// $("button").on("click", function () {
+//     $("h1").fadeOut();
+// });
 
-// Adding an eventListener with a for loop to buttons that changes the h1 color using Vanilla JS:
-// for(i = 0; i < 5; i++) {
-//     document.querySelectorAll("button")[i].addEventListener("click", function() {
-//         document.querySelector("h1").style.color = "green";
-//     })
-// }
+// $("button").on("click", function () {
+//     $("h1").fadeToggle();
+// });
 
-// Using jQuery to change the h1 color when clicking a button:
-$("button").click(function() {
-    $("h1").css("color", "purple");
-})
+// $("button").on("click", function () {
+//     $("h1").slideUp();
+// });
 
-// Using jQuery to detect key strokes in the input of the html:
-$("input").keydown(function(event) {
-    console.log(event.key);
-})
+// $("button").on("click", function () {
+//     $("h1").slideDown();
+// });
 
-// Challenge -> Update the h1 whenever a key is pressed:
-$(document).keydown(function(event) {
-    $("h1").text(event.key);
-})
+// $("button").on("click", function () {
+//     $("h1").slideToggle();
+// });
 
-// More flexible way of adding an eventListener (Note 2 parameters):
-$("h1").on("mouseover", function() {
-    $("h1").css("color", "purple");
-})
+// $("button").on("click", function () {
+//     $("h1").animate({margin: "20%"});
+// });
+
+// Chaining animations:
+$("button").on("click", function () {
+    $("h1").slideUp().slideDown().animate({opacity: 0.5});
+});
