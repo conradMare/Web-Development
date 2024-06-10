8c11340e02068085ccd54b4d73615a440970d294
conradMare <conradmare4@gmail.com>
2024-06-10 10:19:58 +0200

    2024/06/10(am)
    S19.157_Adding Event Listeners with jQuery

--- Section19_jQuery/jQuery/index.js
+++ Section19_jQuery/jQuery/index.js
@@ -1,9 +1,32 @@
 
-// Get value of attribute (Note 1 parameter):
-// console.log($("img").attr("src"));
+// Adding an eventListener with jQuery:
+// $("h1").click(function() {
+//     $("h1").css("color", "purple");
+// })
 
-// Set value of attribute (Note 2 parameters):
-$("a").attr("href", "https://www.yahoo.com");
+// Adding an eventListener with a for loop to buttons that changes the h1 color using Vanilla JS:
+// for(i = 0; i < 5; i++) {
+//     document.querySelectorAll("button")[i].addEventListener("click", function() {
+//         document.querySelector("h1").style.color = "green";
+//     })
+// }
 
-// REMEMBER -> Class is also an attribute:
-console.log($("h1").attr("class"));
+// Using jQuery to change the h1 color when clicking a button:
+$("button").click(function() {
+    $("h1").css("color", "purple");
+})
+
+// Using jQuery to detect key strokes in the input of the html:
+$("input").keydown(function(event) {
+    console.log(event.key);
+})
+
+// Challenge -> Update the h1 whenever a key is pressed:
+$(document).keydown(function(event) {
+    $("h1").text(event.key);
+})
+
+// More flexible way of adding an eventListener (Note 2 parameters):
+$("h1").on("mouseover", function() {
+    $("h1").css("color", "purple");
+})
