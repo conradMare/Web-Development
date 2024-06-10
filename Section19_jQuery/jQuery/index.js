8df313888e8aa0aecd5f10069c19bb77e96c00b2
conradMare <conradmare4@gmail.com>
2024-06-10 09:40:58 +0200

    2024/06/10(am)
    S19.154_Manipulating Text with jQuery

--- Section19_jQuery/jQuery/index.js
+++ Section19_jQuery/jQuery/index.js
@@ -1,22 +1,6 @@
 
-// Add element css property (2 parameters):
-//  $("h1").css("color", "green");
+// Adding text to an element:
+$("h1").text("Bye");
 
-// Retrieve element css property (1 parameter):
-// console.log($("h1").css("font-size"));
-
-// BEST PRACTICE -> SEPARATE CONCERNS!!!
-
-// Add class from "styles.css":
-// $("h1").addClass("big-title");
-
-// Remove class:
-// $("h1").removeClass("big-title");
-
-// Add multiple classes (Note the space in addClass):
-$("h1").addClass("big-title margin-50");
-
-// To see if a particular element has a class:
-$("h1").hasClass("margin-50");
-
-$("button");
+// Instead of writing .innerHTML with JS:
+$("button").html("<em>Hey</em>");
