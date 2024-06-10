1a2b95b50f2a6370cbcbf6deb5b4155729044b3a
conradMare <conradmare4@gmail.com>
2024-06-10 09:24:59 +0200

    2024/06/10(am)
    S19.153_Manipulating Styles with jQuery

--- Section19_jQuery/jQuery/index.js
+++ Section19_jQuery/jQuery/index.js
@@ -1,8 +1,16 @@
 
-// JS -> document.querySelector("h1");
-$("h1");
+// console.log($("h1").css("font-size"));
 
-// Selecting multiple items - JS -> document.querySelectorAll("button");
+// Add class from "styles.css":
+// $("h1").addClass("big-title");
+
+// Remove class:
+// $("h1").removeClass("big-title");
+
+// Add multiple classes (Note the space in addClass):
+$("h1").addClass("big-title margin-50");
+
+// To see if a particular element has a class:
+$("h1").hasClass("margin-50");
+
 $("button");
-
-// jQuery - There is NO DIFFERENCE selecting one element OR many elements!
