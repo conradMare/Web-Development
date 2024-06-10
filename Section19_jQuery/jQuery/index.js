3582cd990d5e5e16963c9d3f28e400522341faf1
conradMare <conradmare4@gmail.com>
2024-06-10 08:54:58 +0200

    2024/06/10(am)
    S19.151_How Minification works to Reduce File Sizes

--- Section19_jQuery/jQuery/index.js
+++ Section19_jQuery/jQuery/index.js
@@ -1,7 +1,2 @@
 
 $("h1").css("color", "red");
-
-// To check if jQuery is ready (but not necessary if script tag has been added in index.html)
-// $(document).ready(function () {
-//     $("h1").css("color", "red");
-// })
