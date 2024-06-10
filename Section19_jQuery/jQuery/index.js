e34c0c647e43e7038db097ae8c8af47795b535ff
conradMare <conradmare4@gmail.com>
2024-06-10 09:02:53 +0200

    2024/06/10(am)
    S19.152_Selecting Elements with jQuery

--- Section19_jQuery/jQuery/index.js
+++ Section19_jQuery/jQuery/index.js
@@ -1,2 +1,8 @@
 
-$("h1").css("color", "red");
+// JS -> document.querySelector("h1");
+$("h1");
+
+// Selecting multiple items - JS -> document.querySelectorAll("button");
+$("button");
+
+// jQuery - There is NO DIFFERENCE selecting one element OR many elements!
