6ac2e8044f6b9a24b203798c8e1edec01d03f11c
conradMare <conradmare4@gmail.com>
2024-06-10 08:47:19 +0200

    2024/06/10(am)
    S19.150_How to Incorporate jQuery into Websites

---
+++ Section19_jQuery/jQuery/index.js
@@ -0,0 +1,7 @@
+
+$("h1").css("color", "red");
+
+// To check if jQuery is ready (but not necessary if script tag has been added in index.html)
+// $(document).ready(function () {
+//     $("h1").css("color", "red");
+// })
