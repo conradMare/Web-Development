e79babd010473b6095f9a236314d6871c358a11b
conradMare <conradmare4@gmail.com>
2024-06-11 06:27:28 +0200

    2024/06/11(am)
    S20.173_Step 6 - Add Animations to User Clicks

--- Section20_BossLevelChallenge2-TheSimonGame/Simon Game Challenge Starting Files/game.js
+++ Section20_BossLevelChallenge2-TheSimonGame/Simon Game Challenge Starting Files/game.js
@@ -4,28 +4,37 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
-$(".btn").click(function () {
+$(".btn").click(function() {
 
-    var userChosenColour = $(this).attr("id");
-    userClickedPattern.push(userChosenColour);
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
 
-    playSound(userChosenColour);
+  playSound(userChosenColour);
 
+  animatePress(userChosenColour);
 });
 
 function nextSequence() {
 
-    var randomNumber = Math.floor(Math.random() * 4);
-    var randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColour);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
 
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    playSound(randomChosenColour);
+  playSound(randomChosenColour);
 }
 
 function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
 
-    var audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
+function animatePress(currentColor) {
+
+  $("#" + currentColor).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 }
