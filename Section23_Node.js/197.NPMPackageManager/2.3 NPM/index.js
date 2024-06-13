6cf0a354957b4e8edf7e9db05f75b6cb929ac2a8
conradMare <conradmare4@gmail.com>
2024-06-13 05:43:55 +0200

    2024/06/13(am)
    S22.197_The NPM Package Manager and Installing External Modules

---
+++ Section23_Node.js/197.NPMPackageManager/2.3 NPM/index.js
@@ -0,0 +1,12 @@
+// var generateName = require("sillyname");
+
+// import generateName from "sillyname";
+// var sillyname = generateName();
+
+// console.log(`My name is ${sillyname}.`);
+
+import superheroes from 'superheroes';
+
+const name = superheroes.random();
+
+console.log(`My superhero name is ${name}!`);
