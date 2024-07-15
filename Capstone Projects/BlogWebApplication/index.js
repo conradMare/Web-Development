8ff6e2439481ec724728597491fa52662e13e610
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-15 05:14:08 +0200

    2024/07/15(am)
    Blog Web Application - Express file path -> index.html

--- Capstone Projects/BlogWebApplication/index.js
+++ Capstone Projects/BlogWebApplication/index.js
@@ -1,20 +1,15 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req,res) => {
-    res.send("<h1>Home Page</h1><p>Welcome to my Blog Web Application</p>");
-})
-
-app.get("/create", (req, res) => {
-    res.send("<h1>Create a Blog post</h1>");
-})
-
-app.get("/entry", (req,res) => {
-    res.send("<h1>Blog Entries</h1><p>List of blog entries</p>");
-})
+    res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-})
+});
