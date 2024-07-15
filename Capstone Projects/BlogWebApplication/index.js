5a8c78fa8cca4a811c5e02bdb4f0a71e57a96182
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-15 05:03:51 +0200

    2024/07/15(am)
    Blog Web Application - index.html, styles.css

--- Capstone Projects/BlogWebApplication/index.js
+++ Capstone Projects/BlogWebApplication/index.js
@@ -4,11 +4,15 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req,res) => {
-    res.send("<h1>Home Page</h1>");
+    res.send("<h1>Home Page</h1><p>Welcome to my Blog Web Application</p>");
+})
+
+app.get("/create", (req, res) => {
+    res.send("<h1>Create a Blog post</h1>");
 })
 
 app.get("/entry", (req,res) => {
-    res.send("<h1>Blog Entries</h1>");
+    res.send("<h1>Blog Entries</h1><p>List of blog entries</p>");
 })
 
 app.listen(port, () => {
