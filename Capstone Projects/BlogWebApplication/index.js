06ea37c572e2541961f6e02e22625dcc217f2861
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-15 07:03:22 +0200

    2024/07/15(am)
    Blog Web Application - Project changed to Node, Express and EJS

--- Capstone Projects/BlogWebApplication/index.js
+++ Capstone Projects/BlogWebApplication/index.js
@@ -1,24 +1,16 @@
 import express from "express";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req,res) => {
-    res.sendFile(__dirname + "/public/index.html");
-});
-
-app.post("/submit", (req,res) => {
-    console.log(req.body);
-});
+    res.render("index.ejs");
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-});
+})
